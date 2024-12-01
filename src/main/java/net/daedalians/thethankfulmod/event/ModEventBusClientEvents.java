2d09245d65cb1854a9e12a026f917e68d5d47c37
J0C-user <callejonathan453@gmail.com>
2024-12-01 00:10:32 -0500

    Created Turkey Boss with attack animation

--- src/main/java/net/daedalians/thethankfulmod/event/ModEventBusClientEvents.java
+++ src/main/java/net/daedalians/thethankfulmod/event/ModEventBusClientEvents.java
@@ -2,6 +2,7 @@ package net.daedalians.thethankfulmod.event;
 
 import net.daedalians.thethankfulmod.TheThankfulMod;
 import net.daedalians.thethankfulmod.entity.client.ModModelLayers;
+import net.daedalians.thethankfulmod.entity.client.TurkeyBossModel;
 import net.daedalians.thethankfulmod.entity.client.TurkeyModel;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
@@ -13,5 +14,6 @@ public class ModEventBusClientEvents {
     @SubscribeEvent
     public static void registerLayer(EntityRenderersEvent.RegisterLayerDefinitions event){
         event.registerLayerDefinition(ModModelLayers.TURKEY_LAYER, TurkeyModel::createBodyLayer);
+        event.registerLayerDefinition(ModModelLayers.TURKEY_BOSS_LAYER, TurkeyBossModel::createBodyLayer);
     }
 }
